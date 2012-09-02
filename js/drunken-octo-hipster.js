c60f028b2d4e7f8793ea7d3b2d138b22c05cedc8
Matt <mmorri77@msudenver.edu>
2012-09-02 13:10:18 -0600

    Adding functions to create/handle behavior
    Added a pseudoGravity function to slowly slide objects downward.  Added
    a keypress handler--pressing 'r' now resets the app.

--- js/drunken-octo-hipster.js
+++ js/drunken-octo-hipster.js
@@ -23,39 +23,66 @@
  */
 
 /*
- * Declarations
+   Declarations:  
+   
  */
- var circle;
+ var circle; 
 
 /*
- *  init()
- *  This function acts as an initialization routine for our game.  
+   init()
+   This function acts as an initialization routine for our game.  
  */
 function init() {
 
-	//Let's begin with a couple of simple shapes to play with.  
-	/*
-		First, we'll make a simple red circle.  In Paper.js, we have an object prototype 
-		called 'Path' to use to draw with.  Paths in Paper are just like paths you might've 
-		seen in drawing programs, such as Illustrator.  A path is a collection of points connected 
-		by curves.  (Note:  a 'curve' might have a curvature of zero--i.e. a straight line.)
-		
-		The path object comes with some handy built-in constructors for common shapes.  Here, we're 
-		going to use the Path.Circle(c,r) function, where c is the center and r is the radius.  
-	*/
-	circle = new Path.Circle(view.center, 33);
-	circle.fillColor = 'red';  // fill is the area encapsulated by a closed path
-	circle.strokeColor = 'black';  // stroke is the border area of the path
-	circle.strokeWidth = 3;  // thickness of stroke
+    //Let's begin with a couple of simple shapes to play with.  
+    /*
+        First, we'll make a simple red circle.  In Paper.js, we have an object prototype 
+        called 'Path' to use to draw with.  Paths in Paper are just like paths you might've 
+        seen in drawing programs, such as Illustrator.  A path is a collection of points connected 
+        by curves.  (Note:  a 'curve' might have a curvature of zero--i.e. a straight line.)
+        
+        The path object comes with some handy built-in constructors for common shapes.  Here, we're 
+        going to use the Path.Circle(c,r) function, where c is the center and r is the radius.  
+    */
+    circle = new Path.Circle(view.center, 33);
+    circle.fillColor = 'red';  // fill is the area encapsulated by a closed path
+    circle.strokeColor = 'black';  // stroke is the border area of the path
+    circle.strokeWidth = 3;  // thickness of stroke
 }
 
 /*
- *  onFrame(event)
- *  This function is a Paper.js built-in to assist with animation.  onFrame() is simply 
- *  a continuous loop that will update up to sixty times/frames per second.  
+    onFrame(event)
+    This function is a Paper.js built-in to assist with animation.  onFrame() is  
+    a continuous loop that will update up to sixty times/frames per second.  
  */
 function onFrame(event) {
-	circle.fillColor.hue += 3;
+    circle.fillColor.hue += 3;   // cycle the hue of the circle's fillColor
+    pseudoGravity(project.activeLayer);  // a cheap, fake, gravity generator
+}
+
+/*
+    onKeyUp(event)
+    Respond to KeyUp events--i.e. releasing a pressed key
+ */
+function onKeyUp(event) {
+    /*
+        We'll just make a simple reset button to show how key events work.  
+    */
+    if (event.key == 'r') {
+        circle.remove();
+        init();
+    }
+}
+
+/*
+    pseudoGravity(layer)
+    Make everything go down.  Note:  this does not take into account any sort of inertia, 
+    acceleration, or other physics--just something to get us started...
+ */
+function pseudoGravity(layer) {
+    for (var i = 0;i < layer.children.length;i++) {
+        layer.children[i].position.y += 1;
+    }
 }
 
 /*
